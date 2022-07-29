7c9bb37be2d84621b6bee31c78b5b4c893030be3
Luis Carlos <luiscarlossantos.lcs.16@gmail.com>
2022-07-29 20:49:03 -0300

    pequeno ajuste na variavel $querycategoria

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -9,20 +9,19 @@ class AdminController extends Controller
 {
     public function indexAdmin($id = 0){
         $data = [];
-/*
+
         $p = new produto();
 
         if($id != 0){
             $p = produto::find($id);
         }                               
-        $queryproduto = produto::join("categorias", "produtos.categoria_id", "=", "categoria.id_categoria");
+        $queryproduto = produto::join("categorias", "produtos.categoria_id", "=", "categorias.id_categoria");
 
         $data["produtos"] = $p;
         $querycategoria = new categoria();
-        $querycategoria = $querycategoria->orderBy("nome_categoria.categorias");
+        $querycategoria = $querycategoria->orderBy("nome_categoria");
         $data["listaCategorias"] = $querycategoria->get(['id_categoria','nome_categoria', 'descricao_categoria']);
         $data["listaProdutos"] = $queryproduto->get(['id_produto', 'nome_produto', 'preco', 'foto', 'descricao_produto', 'situacao', 'categoria_id']);
-        */
 
         return view("admin/home", $data);
     }
